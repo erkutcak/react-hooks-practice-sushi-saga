6c0ba067dd92deead359b49e8e0ebd4e7d036426
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-03-21 20:18:02 +0300

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
-  return (
+
+  const [sushis, setSushis] = useState([])
+  const [money, setMoney] = useState(300)
+
+  useEffect(() => {
+    fetch (API)
+    .then((response) => response.json())
+    .then ((sushiList) => {
+      const newSushiArray = sushiList.map((sushiPlate) => {
+        return { ...sushiPlate, isEaten: false}
+      })
+      setSushis(newSushiArray)
+    })
+  }, [])
+
+  function sushiEaten(clickSushi) {
+    if(money >= clickSushi.price) {
+        const sushiUpdate = sushis.map((sushi) => {
+        if (clickSushi.id === sushi.id) {
+          setMoney(money - sushi.price)
+        return {...sushi, isEaten: true};
+      } else {
+        return sushi
+      }
+    });
+    setSushis(sushiUpdate);
+    } else {
+      alert('You are broke!!!!')
+    }
+  };
+
+  const filteredSuhsi = sushis.filter(sushi => sushi.isEaten)
+  
+return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} sushiEaten={sushiEaten}/>
+      <Table money={money} plates={filteredSuhsi}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, sushiEaten}) {
+
+  const [sushiIndex, setSushiIndex] = useState(0)
+
+  const cutSushi = sushis.slice(sushiIndex, sushiIndex + 4)
+
+  const changeSushiIndex = () => {
+    setSushiIndex((sushiIndex + 4) % sushis.length)
+  }
+
+  const displaySushis = cutSushi.map((sushi) => {
+    return <Sushi key={sushi.id} sushi={sushi} sushiEaten={sushiEaten} />
+  })
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {displaySushis}
+      <MoreButton changeSushiIndex={changeSushiIndex}/>
     </div>
   );
 }
